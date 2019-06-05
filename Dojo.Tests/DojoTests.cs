0b2203e34c8946b7c099570750531140deb62fbe
adrian-trif-birdz <adrian.trif.ext@veolia.com>
2019-06-05 13:16:39 +0200

    first commit

---
+++ Dojo.Tests/DojoTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Dojo.Tests
+{
+    [TestFixture]
+    public class DojoTests
+    {
+        [Test]
+        public void Add_ZeroAndZero_ShouldBeZero()
+        {
+            Assert.AreEqual(0, Dojo.Add(0, 0));
+        }
+    }
+}
---
+++ Dojo/Dojo.cs
@@ -0,0 +1,10 @@
+﻿namespace Dojo
+{
+    public class Dojo
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
